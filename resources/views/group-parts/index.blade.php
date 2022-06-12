8f6b9843a1c923fdd066a40677eda4e4d3c14e41
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-06-12 11:44:46 +0430

    Some changes in group parts view

--- resources/views/group-parts/index.blade.php
+++ resources/views/group-parts/index.blade.php
@@ -26,6 +26,19 @@
                     </a>
                 </div>
             </li>
+            <li>
+                <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd"
+                              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                    <a href="{{ route('group.parts',$group->id) }}"
+                       class="mr-2 text-xs md:text-sm font-medium text-gray-500 hover:text-gray-900">
+                        لیست قطعات گروه {{ $group->name }}
+                    </a>
+                </div>
+            </li>
             <li aria-current="page">
                 <div class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -34,7 +47,7 @@
                               clip-rule="evenodd"/>
                     </svg>
                     <span class="mr-2 text-xs md:text-sm font-medium text-gray-400">
-                        ایجاد قطعه برای گروه {{ $group->name }}
+                        افزودن قطعه به گروه {{ $group->name }}
                     </span>
                 </div>
             </li>
