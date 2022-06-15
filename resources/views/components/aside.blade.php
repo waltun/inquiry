ed8b9d3640531b9bf8953ed9d4d6ac90625a06c4
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-06-15 12:04:08 +0430

    Add collection parts to aside

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -46,7 +46,7 @@
                 <div x-data="{open:false}">
                     <div
                         class="flex items-center text-gray-500 p-2 hover:bg-gray-100 rounded-md hover:text-black cursor-pointer
-                            {{ isActive(['parts.index','parts.create','parts.edit','parts.price.index','parts.price.edit']) }}"
+                            {{ isActive(['parts.index','parts.create','parts.edit','parts.collection','parts.price.index','parts.price.edit']) }}"
                         @click="open = !open">
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
@@ -73,6 +73,10 @@
                             <a href="{{ route('parts.index') }}" class="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100
                             hover:text-black rounded-md {{ isActive('parts.index') }}">
                                 مدیریت قطعات
+                            </a>
+                            <a href="{{ route('parts.collection') }}" class="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100
+                            hover:text-black rounded-md {{ isActive('parts.collection') }}">
+                                مدیریت قطعات مجموعه ای
                             </a>
                             <a href="{{ route('parts.create') }}" class="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100
                             hover:text-black rounded-md {{ isActive('parts.create') }}">
