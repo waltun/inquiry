4ff5342eb1de8da48a37ac9cd12119ad5a7aa129
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-06-15 13:35:23 +0430

    Create part_part table to save collection parts

---
+++ database/migrations/2022_06_15_133236_create_part_part_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('part_part', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('part_id');
+            $table->unsignedBigInteger('part_collection_id');
+        });
+
+        Schema::table('part_part', function (Blueprint $table) {
+            $table->foreign('part_id')->references('id')->on('parts');
+            $table->foreign('part_collection_id')->references('id')->on('parts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('part_part');
+    }
+};
