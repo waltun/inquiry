a87d5ec497380643a31ee0d3100fa1053e779857
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-06-16 17:51:26 +0430

    Create new migration for store inquiry,model and group ids

--- database/migrations/2022_05_28_145844_create_inquiries_table.php
+++ database/migrations/2022_05_28_145844_create_inquiries_table.php
@@ -19,15 +19,12 @@ return new class extends Migration {
             $table->string('manager');
             $table->string('inquiry_number');
             $table->bigInteger('price')->default(0);
-            $table->float('percent')->default(0);
 
             $table->timestamp('archive_at')->nullable();
 
             $table->boolean('submit')->default(false);
 
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('group_id');
-            $table->unsignedBigInteger('model_id');
 
             $table->timestamps();
         });
---
+++ database/migrations/2022_06_16_174344_create_group_model_inquiry_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_model_inquiry', function (Blueprint $table) {
+            $table->id();
+
+            $table->float('percent')->default(0);
+
+            $table->unsignedBigInteger('group_id');
+            $table->unsignedBigInteger('model_id');
+            $table->unsignedBigInteger('inquiry_id');
+
+            $table->index(['group_id', 'model_id', 'inquiry_id']);
+        });
+
+        Schema::table('group_model_inquiry', function (Blueprint $table) {
+            $table->foreign('group_id')->references('id')->on('groups');
+            $table->foreign('model_id')->references('id')->on('modells');
+            $table->foreign('inquiry_id')->references('id')->on('inquiries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_model_inquiry');
+    }
+};
