4a16b98faa85892189fdfbd13c84d21f04c9c76b
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-06-28 10:43:59 +0430

    Create modell part table - migration

---
+++ database/migrations/2022_06_28_104211_create_modell_part_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modell_part', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('model_id');
+            $table->unsignedBigInteger('part_id');
+
+            $table->index(['model_id', 'part_id']);
+        });
+
+        Schema::table('modell_part', function (Blueprint $table) {
+            $table->foreign('model_id')->references('id')->on('modells')->onDelete('cascade');
+            $table->foreign('part_id')->references('id')->on('parts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modell_part');
+    }
+};
