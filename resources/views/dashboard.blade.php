635e371e2dbf6a8c7b5644aa4559d1993f70b3b7
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-08-19 14:31:42 +0430

    Create new quick access in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -77,8 +77,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
                          fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/>
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                              d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"/>
                     </svg>
                 </div>
                 <div>
@@ -96,18 +95,43 @@
             </a>
         </div>
 
-        <div class="bg-red-500 rounded-md p-4 shadow-md">
-            <a href="{{ route('groups.index') }}" class="flex justify-between items-center group">
+        <div class="col-span-2">
+            <div class="bg-red-500 rounded-md p-4 shadow-md">
+                <a href="{{ route('groups.index') }}" class="flex justify-between items-center group">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
+                             fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2"/>
+                        </svg>
+                    </div>
+                    <div>
+                        <p class="text-lg text-black font-medium group-hover:text-white">
+                            مدیریت گروه ها
+                        </p>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
+                             fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7"/>
+                        </svg>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="bg-gray-300 rounded-md p-4 shadow-md">
+            <a href="{{ route('users.index') }}" class="flex justify-between items-center group">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
                          fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2"/>
+                              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
                     </svg>
                 </div>
                 <div>
                     <p class="text-lg text-black font-medium group-hover:text-white">
-                        مدیریت گروه ها
+                        مدیریت کاربران
                     </p>
                 </div>
                 <div>
@@ -119,7 +143,8 @@
                 </div>
             </a>
         </div>
-        <div class="bg-teal-500 rounded-md p-4 shadow-md">
+
+        <div class="bg-teal-400 rounded-md p-4 shadow-md">
             <a href="{{ route('inquiries.index') }}" class="flex justify-between items-center group">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
@@ -142,18 +167,41 @@
                 </div>
             </a>
         </div>
-        <div class="bg-gray-500 rounded-md p-4 shadow-md">
-            <a href="{{ route('users.index') }}" class="flex justify-between items-center group">
+        <div class="bg-teal-500 rounded-md p-4 shadow-md">
+            <a href="{{ route('inquiries.submitted') }}" class="flex justify-between items-center group">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
                          fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
+                              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
                     </svg>
                 </div>
                 <div>
                     <p class="text-lg text-black font-medium group-hover:text-white">
-                        مدیریت کاربران
+                        استعلام های منتظر قیمت
+                    </p>
+                </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
+                         fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7"/>
+                    </svg>
+                </div>
+            </a>
+        </div>
+        <div class="bg-teal-600 rounded-md p-4 shadow-md">
+            <a href="{{ route('inquiries.priced') }}" class="flex justify-between items-center group">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-black group-hover:text-white"
+                         fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                    </svg>
+                </div>
+                <div>
+                    <p class="text-lg text-black font-medium group-hover:text-white">
+                        استعلام های قیمت گذاری شده
                     </p>
                 </div>
                 <div>
@@ -177,7 +225,8 @@
             </div>
             @if(!$inquiries->isEmpty())
                 @foreach($inquiries as $inquiry)
-                    <a href="{{ route('inquiries.index') }}" class="block py-6 px-4 border border-gray-200 shadow-sm rounded-md">
+                    <a href="{{ route('inquiries.index') }}"
+                       class="block py-6 px-4 border border-gray-200 shadow-sm rounded-md">
                         <div class="flex items-center justify-between">
                             <div class="flex items-center space-x-3 space-x-reverse">
                                 <p class="text-sm text-gray-600">
@@ -212,7 +261,8 @@
             </div>
             @if(!$submitInquiries->isEmpty())
                 @foreach($submitInquiries as $inquiry)
-                    <a href="{{ route('inquiries.submitted') }}" class="block py-6 px-4 border border-gray-200 shadow-sm rounded-md">
+                    <a href="{{ route('inquiries.submitted') }}"
+                       class="block py-6 px-4 border border-gray-200 shadow-sm rounded-md">
                         <div class="flex items-center justify-between">
                             <div class="flex items-center space-x-3 space-x-reverse">
                                 <p class="text-sm text-gray-600">
