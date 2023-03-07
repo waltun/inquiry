2fb5395cae8604f6c02ffc19ecd294bea21cb42c
pooriya <pooriyamostaan1@gmail.com>
2023-03-07 14:04:23 +0330

    Change view of users

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,45 +1,58 @@
 <x-layout>
     <!-- Breadcrumb -->
-    <nav class="flex bg-gray-100 p-4 rounded-md overflow-x-auto whitespace-nowrap" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-2 space-x-reverse">
-            <li class="inline-flex items-center">
-                <a href="{{ route('dashboard') }}"
-                   class="inline-flex items-center text-xs md:text-sm text-gray-500 hover:text-gray-900">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20"
-                         fill="currentColor">
-                        <path
-                            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
-                    </svg>
+    <div class="flex items-center space-x-2 space-x-reverse">
+        <a href="{{ route('dashboard') }}" class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="breadcrumb-svg">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"/>
+            </svg>
+            <div class="mr-2">
+                <p class="breadcrumb-p">
                     داشبورد
-                </a>
-            </li>
-            <li>
-                <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                              clip-rule="evenodd"/>
-                    </svg>
-                    <a href="{{ route('users.index') }}"
-                       class="mr-2 text-xs md:text-sm font-medium text-gray-500 hover:text-gray-900">
-                        مدیریت کاربران
-                    </a>
-                </div>
-            </li>
-            <li aria-current="page">
-                <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                              clip-rule="evenodd"/>
-                    </svg>
-                    <span class="mr-2 text-xs md:text-sm font-medium text-gray-400">
-                        ایجاد کاربر جدید
-                    </span>
-                </div>
-            </li>
-        </ol>
-    </nav>
+                </p>
+            </div>
+        </a>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                 class="breadcrumb-svg-arrow">
+                <path fill-rule="evenodd"
+                      d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                      clip-rule="evenodd"/>
+            </svg>
+        </div>
+        <a href="{{ route('users.index') }}" class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="breadcrumb-svg" fill="none"
+                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
+            </svg>
+            <div class="mr-2">
+                <p class="breadcrumb-p">
+                    مدیریت کاربران
+                </p>
+            </div>
+        </a>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                 class="breadcrumb-svg-arrow">
+                <path fill-rule="evenodd"
+                      d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                      clip-rule="evenodd"/>
+            </svg>
+        </div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="breadcrumb-svg-active">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+            </svg>
+            <div class="mr-2">
+                <p class="breadcrumb-p-active">
+                    ایجاد کاربر جدید
+                </p>
+            </div>
+        </div>
+    </div>
 
     <!-- Errors -->
     <div class="mt-4">
@@ -50,57 +63,63 @@
     <form method="POST" action="{{ route('users.store') }}" class="md:grid grid-cols-2 gap-4 mt-4">
         @csrf
 
-        <div class="bg-white shadow-sm p-4 rounded-md border border-gray-200 mb-4 md:mb-0">
-            <p class="md:text-sm text-xs text-black font-bold border-b-2 border-teal-400 pb-3">مشخصات کلی</p>
+        <div class="card">
+            <div class="card-header">
+                <p class="card-title">مشخصات کلی</p>
+            </div>
 
             <div class="mt-4">
-                <label for="inputName" class="block mb-2 md:text-sm text-xs text-black">نام و نام خانوادگی</label>
+                <label for="inputName" class="form-label">نام و نام خانوادگی</label>
                 <input type="text" id="inputName" name="name" class="input-text" placeholder="مثال : رضا رضایی"
                        value="{{ old('name') }}">
             </div>
 
             <div class="mt-4">
-                <label for="inputEmail" class="block mb-2 md:text-sm text-xs text-black">ایمیل</label>
+                <label for="inputEmail" class="form-label">ایمیل</label>
                 <input type="email" id="inputEmail" name="email" class="input-text"
                        placeholder="مثال : example@gmail.com" value="{{ old('email') }}">
             </div>
 
             <div class="mt-4">
-                <label for="inputPhone" class="block mb-2 md:text-sm text-xs text-black">شماره تماس</label>
+                <label for="inputPhone" class="form-label">شماره تماس</label>
                 <input type="number" id="inputPhone" name="phone" class="input-text hide-appearance"
                        placeholder="مثال : 09123456789" value="{{ old('phone') }}">
             </div>
 
         </div>
 
-        <div class="bg-white shadow-sm p-4 rounded-md border border-gray-200 mb-4 md:mb-0">
-            <p class="md:text-sm text-xs text-black font-bold border-b-2 border-teal-400 pb-3">رمز عبور</p>
+        <div class="card">
+            <div class="card-header">
+                <p class="card-title">رمز عبور</p>
+            </div>
 
             <div class="mt-4">
-                <label for="inputPassword" class="block mb-2 md:text-sm text-xs text-black">رمز عبور</label>
+                <label for="inputPassword" class="form-label">رمز عبور</label>
                 <input type="password" id="inputPassword" name="password" class="input-text"
                        placeholder="حداقل 8 حرف یا کلمه">
             </div>
 
             <div class="mt-4">
-                <label for="inputPasswordRepeat" class="block mb-2 md:text-sm text-xs text-black">تکرار رمزعبور</label>
+                <label for="inputPasswordRepeat" class="form-label">تکرار رمزعبور</label>
                 <input type="password" id="inputPasswordRepeat" name="password_confirmation" class="input-text"
                        placeholder="باید با رمز عبور یکی باشد">
             </div>
 
         </div>
 
-        <div class="bg-white shadow-sm p-4 rounded-md border border-gray-200 mb-4 md:mb-0">
-            <p class="md:text-sm text-xs text-black font-bold border-b-2 border-teal-400 pb-3">اطلاعات بیشتر</p>
+        <div class="card">
+            <div class="card-header">
+                <p class="card-title">اطلاعات بیشتر</p>
+            </div>
 
             <div class="mt-4">
-                <label for="inputNation" class="block mb-2 md:text-sm text-xs text-black">کد ملی</label>
+                <label for="inputNation" class="form-label">کد ملی</label>
                 <input type="number" id="inputNation" name="nation" class="input-text" placeholder="مثال : 0430521667"
                        value="{{ old('nation') }}">
             </div>
 
             <div class="mt-4">
-                <label for="inputGender" class="block mb-2 md:text-sm text-xs text-black">جنسیت</label>
+                <label for="inputGender" class="form-label">جنسیت</label>
                 <select name="gender" id="inputGender" class="input-text">
                     <option value="">انتخاب کنید</option>
                     <option value="male" {{ old('gender') == 'male' ? 'selected' : '' }}>مرد</option>
@@ -110,11 +129,13 @@
 
         </div>
 
-        <div class="bg-white shadow-sm p-4 rounded-md border border-gray-200 mb-4 md:mb-0">
-            <p class="md:text-sm text-xs text-black font-bold border-b-2 border-teal-400 pb-3">نقش کاربر</p>
+        <div class="card">
+            <div class="card-header">
+                <p class="card-title">نقش کاربر</p>
+            </div>
 
             <div class="mt-4">
-                <label for="inputRole" class="block mb-2 md:text-sm text-xs text-black">نقش (سِمَت)</label>
+                <label for="inputRole" class="form-label">نقش (سِمَت)</label>
                 <select name="role" id="inputRole" class="input-text">
                     <option value="">انتخاب کنید</option>
                     <option value="admin" {{ old('role') == 'admin' ? 'selected' : '' }}>
@@ -130,7 +151,7 @@
             </div>
 
             <div class="mt-4">
-                <label for="inputActive" class="block mb-2 md:text-sm text-xs text-black">تایید کاربر</label>
+                <label for="inputActive" class="form-label">تایید کاربر</label>
                 <select name="active" id="inputActive" class="input-text">
                     <option value="">انتخاب کنید</option>
                     <option value="0" {{ old('active') == '0' ? 'selected' : '' }}>کاربر تایید نشده</option>
@@ -140,7 +161,7 @@
 
         </div>
 
-        <div class="col-span-2 space-x-4 space-x-reverse">
+        <div class="flex items-center space-x-4 space-x-reverse bottom-4 sticky">
             <button type="submit" class="form-submit-btn">
                 ثبت کاربر
             </button>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,117 +1,163 @@
 <x-layout>
     <!-- Breadcrumb -->
-    <nav class="flex bg-gray-100 p-4 rounded-md overflow-x-auto whitespace-nowrap" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-2 space-x-reverse">
-            <li class="inline-flex items-center">
-                <a href="{{ route('dashboard') }}"
-                   class="inline-flex items-center text-xs md:text-sm text-gray-500 hover:text-gray-900">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20"
-                         fill="currentColor">
-                        <path
-                            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
-                    </svg>
+    <div class="flex items-center space-x-2 space-x-reverse">
+        <a href="{{ route('dashboard') }}" class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="breadcrumb-svg">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"/>
+            </svg>
+            <div class="mr-2">
+                <p class="breadcrumb-p">
                     داشبورد
-                </a>
-            </li>
-            <li aria-current="page">
-                <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                              clip-rule="evenodd"/>
-                    </svg>
-                    <span class="mr-2 text-xs md:text-sm font-medium text-gray-400">
-                        مدیریت کاربران
-                    </span>
-                </div>
-            </li>
-        </ol>
-    </nav>
-
-    <!-- Navigation Btn -->
-    <div class="mt-4 flex justify-between items-center space-x-4 space-x-reverse">
+                </p>
+            </div>
+        </a>
         <div>
-            <p class="text-lg text-black font-bold">
-                لیست کاربران
-            </p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                 class="breadcrumb-svg-arrow">
+                <path fill-rule="evenodd"
+                      d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                      clip-rule="evenodd"/>
+            </svg>
         </div>
-        <div class="flex items-center space-x-2 space-x-reverse">
-            <a href="{{ route('users.create') }}" class="form-submit-btn text-xs">ایجاد کاربر جدید</a>
-            <a href="{{ route('users.deleted') }}" class="form-cancel-btn text-xs">کاربران حذف شده</a>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="breadcrumb-svg-active" fill="none"
+                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
+            </svg>
+            <div class="mr-2">
+                <p class="breadcrumb-p-active">
+                    مدیریت کاربران
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Navigation -->
+    <div class="flex items-center justify-between mt-8">
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 dark:text-white" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
+            </svg>
+            <div class="mr-2">
+                <p class="font-bold text-2xl text-black dark:text-white">
+                    لیست کاربران
+                </p>
+            </div>
+        </div>
+        <div class="flex items-center space-x-4 space-x-reverse">
+            <a href="{{ route('users.create') }}" class="page-success-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                </svg>
+                <span class="mr-2">ایجاد کاربر جدید</span>
+            </a>
         </div>
     </div>
 
     <!-- Content -->
-    <div class="mt-4">
-        <!-- Laptop List -->
-        <div class="bg-white shadow overflow-x-auto rounded-lg hidden md:block">
-            <table class="min-w-full">
+    <div class="mt-4 space-y-4">
+        <div class="mt-8 overflow-x-auto rounded-lg">
+            <table class="w-full border-collapse">
                 <thead>
-                <tr class="bg-sky-200">
-                    <th scope="col"
-                        class="px-4 py-3 text-sm font-bold text-gray-800 text-center rounded-r-md">
+                <tr class="table-th-tr">
+                    <th scope="col" class="p-4 rounded-tr-lg">
                         #
                     </th>
-                    <th scope="col" class="px-4 py-3 text-sm font-bold text-gray-800 text-center">
+                    <th scope="col" class="p-4">
                         نام
                     </th>
-                    <th scope="col" class="px-4 py-3 text-sm font-bold text-gray-800 text-center">
+                    <th scope="col" class="p-4">
                         شماره تماس
                     </th>
-                    <th scope="col" class="px-4 py-3 text-sm font-bold text-gray-800 text-center">
+                    <th scope="col" class="p-4">
                         ایمیل
                     </th>
-                    <th scope="col" class="px-4 py-3 text-sm font-bold text-gray-800 text-center">
+                    <th scope="col" class="p-4">
                         دسترسی ها
                     </th>
-                    <th scope="col" class="relative px-4 py-3 rounded-l-md">
+                    <th scope="col" class="p-4 rounded-tl-lg">
                         <span class="sr-only">اقدامات</span>
                     </th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($users as $user)
-                    <tr>
-                        <td class="px-4 py-3 whitespace-nowrap">
-                            <p class="text-sm text-gray-500 text-center">{{ $loop->index + 1 }}</p>
+                    <tr class="table-tb-tr group">
+                        <td class="table-tr-td border-t-0 border-l-0">
+                            {{ $loop->index + 1 }}
                         </td>
-                        <td class="px-4 py-3 whitespace-nowrap">
-                            <p class="text-sm text-black text-center">{{ $user->name }}</p>
+                        <td class="table-tr-td border-t-0 border-x-0">
+                            {{ $user->name }}
                         </td>
-                        <td class="px-4 py-3 whitespace-nowrap">
-                            <p class="text-sm text-black text-center">{{ $user->phone }}</p>
+                        <td class="table-tr-td border-t-0 border-x-0">
+                            {{ $user->phone }}
                         </td>
-                        <td class="px-4 py-3 whitespace-nowrap">
-                            <p class="text-sm text-black text-center">{{ $user->email }}</p>
+                        <td class="table-tr-td border-t-0 border-x-0">
+                            {{ $user->email }}
                         </td>
-                        <td class="px-4 py-3 whitespace-nowrap">
+                        <td class="table-tr-td border-t-0 border-x-0">
                             <div class="flex justify-center">
                                 @if($user->role != 'admin')
                                     <a href="{{ route('users.permissions',$user->id) }}"
-                                       class="form-detail-btn text-xs">
+                                       class="table-warning-btn">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-1">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                  d="M12 9v3.75m0-10.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.249-8.25-3.286zm0 13.036h.008v.008H12v-.008z"/>
+                                        </svg>
                                         دسترسی ها
                                     </a>
                                 @else
-                                    <p class="text-sm font-medium text-black">
-                                        مدیر
-                                    </p>
+                                    مدیر
                                 @endif
                             </div>
                         </td>
-                        <td class="px-4 py-3 space-x-3 space-x-reverse whitespace-nowrap">
-                            <a href="{{ route('users.edit',$user->id) }}" class="form-edit-btn text-xs">
-                                ویرایش
-                            </a>
-                            @if($delete->users)
-                                <form action="{{ route('users.destroy',$user->id) }}" method="POST"
-                                      class="inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="form-cancel-btn text-xs" onclick="return confirm('کاربر حذف شود ؟')">
-                                        حذف
-                                    </button>
-                                </form>
-                            @endif
+                        <td class="table-tr-td border-t-0 border-r-0 whitespace-nowrap">
+                            <div class="flex items-center justify-center space-x-4 space-x-reverse relative"
+                                 x-data="{open:false}">
+
+                                <button @click="open = !open">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
+                                    </svg>
+                                </button>
+
+                                <div x-show="open" @click.away="open = false" class="table-dropdown -top-12 -right-24"
+                                     x-cloak>
+                                    <a href="{{ route('users.edit',$user->id) }}"
+                                       class="table-dropdown-edit">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-1">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"/>
+                                        </svg>
+                                        ویرایش
+                                    </a>
+                                    @if($delete->parts)
+                                        <form action="{{ route('users.destroy',$user->id) }}" method="POST"
+                                              class="table-dropdown-delete">
+                                            @csrf
+                                            @method('DELETE')
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-1">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
+                                            </svg>
+                                            <button onclick="return confirm('کاربر حذف شود ؟')">
+                                                حذف
+                                            </button>
+                                        </form>
+                                    @endif
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
