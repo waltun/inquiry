5c787608a4c4101e31525f7b3e2647bd3952372b
pooriya <pooriyamostaan1@gmail.com>
2023-03-17 15:55:26 +0330

    Change view of calculations

--- resources/views/calculate/separate-coils/index.blade.php
+++ resources/views/calculate/separate-coils/index.blade.php
@@ -98,5 +98,90 @@
                 </div>
             </a>
         </div>
+        <div class="grid grid-cols-3 gap-4 mt-8">
+            @can('separate-calculate-dampers')
+                <a href="{{ route('separate.damper.index') }}" class="dashboard-cards group">
+                    <div class="flex items-center">
+                        <div class="dashboard-card-icon bg-yellow-600 dark:bg-slate-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 text-white group-hover:text-myBlue-100">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"/>
+                            </svg>
+                        </div>
+                        <div class="mr-4">
+                            <p class="font-bold text-black text-base group-hover:text-white dark:text-white">
+                                محاسبه قیمت دمپر
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                             class="w-5 h-5 text-gray-600 group-hover:text-gray-200 dark:text-white">
+                            <path fill-rule="evenodd"
+                                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                </a>
+            @endcan
+
+            @can('separate-calculate-converters')
+                <a href="{{ route('separate.converter.index') }}" class="dashboard-cards group">
+                    <div class="flex items-center">
+                        <div class="dashboard-card-icon bg-yellow-600 dark:bg-slate-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 text-white group-hover:text-myBlue-100">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"/>
+                            </svg>
+                        </div>
+                        <div class="mr-4">
+                            <p class="font-bold text-black text-base group-hover:text-white dark:text-white">
+                                محاسبه قیمت مبدل
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                             class="w-5 h-5 text-gray-600 group-hover:text-gray-200 dark:text-white">
+                            <path fill-rule="evenodd"
+                                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                </a>
+            @endcan
+
+            @can('separate-calculate-electricals')
+                <a href="{{ route('separate.electrical.index') }}" class="dashboard-cards group">
+                    <div class="flex items-center">
+                        <div class="dashboard-card-icon bg-yellow-600 dark:bg-slate-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 text-white group-hover:text-myBlue-100">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"/>
+                            </svg>
+                        </div>
+                        <div class="mr-4">
+                            <p class="font-bold text-black text-base group-hover:text-white dark:text-white">
+                                محاسبه قیمت تابلوبرق
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                             class="w-5 h-5 text-gray-600 group-hover:text-gray-200 dark:text-white">
+                            <path fill-rule="evenodd"
+                                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                </a>
+            @endcan
+        </div>
     </div>
 </x-layout>
--- resources/views/calculate/separate-electrical/index.blade.php
+++ resources/views/calculate/separate-electrical/index.blade.php
@@ -37,116 +37,152 @@
 
     <!-- Content -->
     <div class="mt-4">
-        <div class="grid grid-cols-4 gap-4">
+        <div class="grid grid-cols-5 gap-4">
             <a href="{{ route('separate.electrical.panel',$panel->id) }}"
-               class="block p-8 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
-                <div class="mb-4">
-                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-full mx-auto">
+               class="block p-4 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
+                <div class="mb-4 border-b border-gray-300 pb-2">
+                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-28 mx-auto">
                 </div>
-                <div class="mb-8">
+                <div>
                     <p class="text-base font-bold text-black group-hover:text-white text-center">
                         {{ $panel->name }}
                     </p>
                 </div>
-                <div class="flex justify-center">
-                    <button type="button" class="table-success-btn group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-4 h-4 ml-1">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"/>
-                        </svg>
-                        محاسبه قیمت
-                    </button>
-                </div>
             </a>
 
             <a href="{{ route('separate.electrical.chiller',$chiller->id) }}"
-               class="block p-8 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
-                <div class="mb-4">
-                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-full mx-auto">
+               class="block p-4 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
+                <div class="mb-4 border-b border-gray-300 pb-2">
+                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-28 mx-auto">
                 </div>
-                <div class="mb-8">
+                <div>
                     <p class="text-base font-bold text-black group-hover:text-white text-center">
                         {{ $chiller->name }}
                     </p>
                 </div>
-                <div class="flex justify-center">
-                    <button type="button" class="table-success-btn group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-4 h-4 ml-1">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"/>
-                        </svg>
-                        محاسبه قیمت
-                    </button>
-                </div>
             </a>
 
             <a href="{{ route('separate.electrical.air',$air->id) }}"
-               class="block p-8 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
-                <div class="mb-4">
-                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-full mx-auto">
+               class="block p-4 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
+                <div class="mb-4 border-b border-gray-300 pb-2">
+                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-28 mx-auto">
                 </div>
-                <div class="mb-8">
+                <div>
                     <p class="text-base font-bold text-black group-hover:text-white text-center">
                         {{ $air->name }}
                     </p>
                 </div>
-                <div class="flex justify-center">
-                    <button type="button" class="table-success-btn group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-4 h-4 ml-1">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"/>
-                        </svg>
-                        محاسبه قیمت
-                    </button>
-                </div>
             </a>
 
             <a href="{{ route('separate.electrical.zent',$zent->id) }}"
-               class="block p-8 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
-                <div class="mb-4">
-                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-full mx-auto">
+               class="block p-4 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
+                <div class="mb-4 border-b border-gray-300 pb-2">
+                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-28 mx-auto">
                 </div>
-                <div class="mb-8">
+                <div>
                     <p class="text-base font-bold text-black group-hover:text-white text-center">
                         {{ $zent->name }}
                     </p>
                 </div>
-                <div class="flex justify-center">
-                    <button type="button" class="table-success-btn group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-4 h-4 ml-1">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"/>
-                        </svg>
-                        محاسبه قیمت
-                    </button>
-                </div>
             </a>
 
             <a href="{{ route('separate.electrical.mini',$mini->id) }}"
-               class="block p-8 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
-                <div class="mb-4">
-                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-full mx-auto">
+               class="block p-4 rounded-lg shadow-lg border border-gray-200 group hover:bg-indigo-700 hover:shadow-2xl hover:border-transparent">
+                <div class="mb-4 border-b border-gray-300 pb-2">
+                    <img src="{{ asset('images/electrical.png') }}" alt="" class="w-28 mx-auto">
                 </div>
-                <div class="mb-8">
+                <div>
                     <p class="text-base font-bold text-black group-hover:text-white text-center">
                         {{ $mini->name }}
                     </p>
                 </div>
-                <div class="flex justify-center">
-                    <button type="button" class="table-success-btn group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-4 h-4 ml-1">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"/>
-                        </svg>
-                        محاسبه قیمت
-                    </button>
-                </div>
             </a>
+        </div>
+
+        <div class="grid grid-cols-3 gap-4 mt-8">
+            @can('separate-calculate-coils')
+                <a href="{{ route('separate.coil.index') }}" class="dashboard-cards group">
+                    <div class="flex items-center">
+                        <div class="dashboard-card-icon bg-yellow-600 dark:bg-slate-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 text-white group-hover:text-myBlue-100">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"/>
+                            </svg>
+                        </div>
+                        <div class="mr-4">
+                            <p class="font-bold text-black text-base group-hover:text-white dark:text-white">
+                                محاسبه قیمت کویل
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                             class="w-5 h-5 text-gray-600 group-hover:text-gray-200 dark:text-white">
+                            <path fill-rule="evenodd"
+                                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                </a>
+            @endcan
+
+            @can('separate-calculate-dampers')
+                <a href="{{ route('separate.damper.index') }}" class="dashboard-cards group">
+                    <div class="flex items-center">
+                        <div class="dashboard-card-icon bg-yellow-600 dark:bg-slate-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 text-white group-hover:text-myBlue-100">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"/>
+                            </svg>
+                        </div>
+                        <div class="mr-4">
+                            <p class="font-bold text-black text-base group-hover:text-white dark:text-white">
+                                محاسبه قیمت دمپر
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                             class="w-5 h-5 text-gray-600 group-hover:text-gray-200 dark:text-white">
+                            <path fill-rule="evenodd"
+                                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                </a>
+            @endcan
+
+            @can('separate-calculate-converters')
+                <a href="{{ route('separate.converter.index') }}" class="dashboard-cards group">
+                    <div class="flex items-center">
+                        <div class="dashboard-card-icon bg-yellow-600 dark:bg-slate-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 text-white group-hover:text-myBlue-100">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"/>
+                            </svg>
+                        </div>
+                        <div class="mr-4">
+                            <p class="font-bold text-black text-base group-hover:text-white dark:text-white">
+                                محاسبه قیمت مبدل
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                             class="w-5 h-5 text-gray-600 group-hover:text-gray-200 dark:text-white">
+                            <path fill-rule="evenodd"
+                                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                </a>
+            @endcan
         </div>
     </div>
 </x-layout>
