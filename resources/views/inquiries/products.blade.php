de6b89961ff39ea34068fb915ade36b094481dd8
pooriya <pooriyamostaan1@gmail.com>
2024-01-29 12:22:04 +0330

    Show submit inquiry in parts

--- resources/views/inquiries/products.blade.php
+++ resources/views/inquiries/products.blade.php
@@ -93,17 +93,17 @@
                                             صدور پیش فاکتور
                                         </h3>
                                         <button type="button" @click="open = false">
-                                                                <span class="modal-close">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                                         fill="none"
-                                                                         viewBox="0 0 24 24"
-                                                                         stroke-width="1.5" stroke="currentColor"
-                                                                         class="w-5 h-5 dark:text-white">
-                                                                        <path stroke-linecap="round"
-                                                                              stroke-linejoin="round"
-                                                                              d="M6 18L18 6M6 6l12 12"/>
-                                                                    </svg>
-                                                                </span>
+                                            <span class="modal-close">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                     fill="none"
+                                                     viewBox="0 0 24 24"
+                                                     stroke-width="1.5" stroke="currentColor"
+                                                     class="w-5 h-5 dark:text-white">
+                                                    <path stroke-linecap="round"
+                                                          stroke-linejoin="round"
+                                                          d="M6 18L18 6M6 6l12 12"/>
+                                                </svg>
+                                            </span>
                                         </button>
                                     </div>
                                     <div class="mt-6">
