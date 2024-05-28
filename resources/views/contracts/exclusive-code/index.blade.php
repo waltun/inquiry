1fc0d388cd50b74ca73c78ea563135764900991f
pooriya <pooriyamostaan1@gmail.com>
2024-05-28 12:59:57 +0330

    Show all model exclusive code in contract

--- resources/views/contracts/exclusive-code/index.blade.php
+++ resources/views/contracts/exclusive-code/index.blade.php
@@ -119,6 +119,9 @@
                                 <th scope="col" class="p-4">
                                     کد اختصاصی
                                 </th>
+                                <th scope="col" class="p-4">
+                                    کد های اختصاصی استفاده شده
+                                </th>
                             </tr>
                             </thead>
                             <tbody>
@@ -143,9 +146,71 @@
                                     <td class="table-tr-td border-t-0 border-x-0">
                                         {{ $product->quantity }}
                                     </td>
-                                    <td class="table-tr-td border-t-0 border-r-0">
+                                    <td class="table-tr-td border-t-0 border-x-0">
                                         <input type="text" name="codes[]" class="input-text w-36 text-center"
-                                            value="{{ $product->code }}">
+                                               value="{{ $modell->exclusive_code ?? $product->code }}">
+                                    </td>
+                                    <td class="table-tr-td border-t-0 border-r-0">
+                                        <div class="flex items-center justify-center" x-data="{open:false}">
+                                            <button class="table-info-btn" @click="open = !open" type="button">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-1">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                          d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"/>
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                                </svg>
+                                                مشاهده
+                                            </button>
+
+                                            <div class="relative z-10" x-show="open" x-cloak>
+                                                <div class="modal-backdrop"></div>
+                                                <div class="fixed z-10 inset-0 overflow-y-auto">
+                                                    <div class="modal">
+                                                        <div class="modal-body">
+                                                            <div class="bg-white dark:bg-slate-800 p-4">
+                                                                <div class="mb-4 flex justify-between items-center">
+                                                                    <h3 class="text-lg font-bold text-gray-900 dark:text-white">
+                                                                          کد های اختصاصی استفاده شده {{ $modell->parent->name }}
+                                                                        - {{ $product->model_custom_name ?? $modell->name }}
+                                                                    </h3>
+                                                                    <button type="button" @click="open = false">
+                                                                    <span class="modal-close">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                                                             fill="none"
+                                                                             viewBox="0 0 24 24"
+                                                                             stroke-width="1.5" stroke="currentColor"
+                                                                             class="w-5 h-5 dark:text-white">
+                                                                            <path stroke-linecap="round"
+                                                                                  stroke-linejoin="round"
+                                                                                  d="M6 18L18 6M6 6l12 12"/>
+                                                                        </svg>
+                                                                    </span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="mt-6 space-y-2">
+                                                                    @php
+                                                                        $allModelProducts = \App\Models\ContractProduct::where('model_id', $modell->id)->get();
+                                                                    @endphp
+                                                                    <div class="mt-4 grid grid-cols-3 gap-4" x-show="open">
+                                                                        @foreach($allModelProducts as $modelProduct)
+                                                                            @if($modelProduct->code)
+                                                                                <div class="px-4 py-2 rounded-md border border-gray-400 shadow inline">
+                                                                                    <p class="text-xs font-medium text-black text-center">
+                                                                                        {{ $modelProduct->code }}
+                                                                                    </p>
+                                                                                    <p class="text-xs mt-2 text-center text-black">
+                                                                                        {{ $modelProduct->contract->number }} | {{ $modelProduct->contract->name }}
+                                                                                    </p>
+                                                                                </div>
+                                                                            @endif
+                                                                        @endforeach
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </td>
                                 </tr>
                             @endforeach
